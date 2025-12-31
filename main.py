1746e45e737b92abbb1d18c1337fc19b6bd70316
Khodirsamy <khodirsamy@gmail.com>
2025-12-31 12:24:22 +0100

    Fix variable environnement Stripe

--- main.py
+++ main.py
@@ -193,7 +193,7 @@ def upgrade_me(
 
 # --- CONFIGURATION STRIPE ---
 # ⚠️ Remplace par TA Clé Secrète (celle qui commence par sk_test_...)
-stripe.api_key = "STRIPE_API_KEY" 
+stripe.api_key = os.getenv("STRIPE_API_KEY")
 
 @app.post("/create-checkout-session")
 def create_checkout_session(current_user: models.User = Depends(get_current_user)):
