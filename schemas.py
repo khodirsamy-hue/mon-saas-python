193966a3d69a79eef42bb9644e00cbdeb107c4d2
Khodirsamy <khodirsamy@gmail.com>
2025-12-31 13:08:13 +0100

    Ajout is_premium dans schemas

--- schemas.py
+++ schemas.py
@@ -1,40 +1,44 @@
 from pydantic import BaseModel
 from typing import List, Optional
 
-# --- SCHÉMAS POUR LES TOKENS (Le badge d'accès) ---
+# --- URL SCHEMAS ---
+class URLBase(BaseModel):
+    url: str
+
+class URLCreate(URLBase):
+    custom_key: Optional[str] = None # Optionnel pour le frontend
+
+class URLItem(URLBase):
+    id: int
+    short_key: str
+    clicks: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+# --- USER SCHEMAS ---
+class UserBase(BaseModel):
+    email: str
+
+class UserCreate(UserBase):
+    password: str
+
+class UserResponse(UserBase):
+    id: int
+    is_premium: bool = False  # <--- VOICI LA LIGNE QUI MANQUAIT !
+    items: List[URLItem] = []
+
+    class Config:
+        orm_mode = True
+
+# --- TOKEN SCHEMAS ---
 class Token(BaseModel):
     access_token: str
     token_type: str
 
 class TokenData(BaseModel):
-    email: str | None = None
+    email: Optional[str] = None
 
-# --- SCHÉMAS POUR LES URLS ---
-class URLBase(BaseModel):
-    url: str
-
-class URLCreate(URLBase):
-    custom_key: str | None = None  # Nouveau : L'utilisateur peut proposer son mot (ou rien)
-
-class URLResponse(URLBase):
-    short_key: str
-    clicks: int
-    
-    class Config:
-        from_attributes = True
-
-# --- SCHÉMAS POUR LES UTILISATEURS ---
-class UserBase(BaseModel):
-    email: str
-
-# Ce que l'utilisateur envoie pour s'inscrire (avec mot de passe)
-class UserCreate(UserBase):
-    password: str
-
-# Ce que l'API renvoie (JAMAIS le mot de passe !)
-class UserResponse(UserBase):
-    id: int
-    items: List[URLResponse] = []
-
-    class Config:
-        from_attributes = True
+class URLResponse(URLItem):
+    pass
