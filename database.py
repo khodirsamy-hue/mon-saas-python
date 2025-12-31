6bea7648d932f82fe839e654ab1459e242249017
Khodirsamy <khodirsamy@gmail.com>
2025-12-31 09:20:42 +0100

    Migration vers PostgreSQL

--- database.py
+++ database.py
@@ -1,17 +1,27 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# 1. On définit le nom de la base de données (ce sera un simple fichier)
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# 1. On cherche l'adresse de la BDD dans les variables d'environnement (Render)
+# Si on ne trouve rien (sur ton PC), on utilise le fichier sqlite local.
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-# 2. On crée le "moteur" de connexion
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+if SQLALCHEMY_DATABASE_URL:
+    # Petit correctif car Render donne une adresse en "postgres://" 
+    # mais SQLAlchemy veut du "postgresql://"
+    if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    
+    # Configuration pour PostgreSQL (Prod)
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+else:
+    # Configuration pour SQLite (Local)
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
 
-# 3. On crée la "session" (l'outil pour envoyer des commandes)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# 4. On crée la base pour nos futurs modèles (tables)
 Base = declarative_base()
