b4c4b1b0d50c086165f739c7fa59d3801a4a1890
Khodirsamy <khodirsamy@gmail.com>
2026-01-01 12:46:37 +0100

    Fix Email SMTP Googlemail

--- main.py
+++ main.py
@@ -251,15 +251,18 @@ def upgrade_me(
     return {"message": "Félicitations ! Vous êtes maintenant membre VIP (Premium) gratuitement."}
 
 # --- CONFIGURATION EMAIL ---
-# On récupère les infos secrètes depuis les variables d'environnement
 mail_conf = ConnectionConfig(
     MAIL_USERNAME = os.getenv("MAIL_USERNAME"),
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD"),
     MAIL_FROM = os.getenv("MAIL_USERNAME"),
-    MAIL_PORT = 465,  # <--- On change le port
-    MAIL_SERVER = "smtp.gmail.com",
-    MAIL_STARTTLS = False, # <--- On désactive le démarrage progressif
-    MAIL_SSL_TLS = True,   # <--- On active le SSL direct
+    
+    # --- ON CHANGE CES 4 LIGNES ---
+    MAIL_PORT = 587,
+    MAIL_SERVER = "smtp.googlemail.com", # Astuce : Cette adresse est souvent plus fiable
+    MAIL_STARTTLS = True, # On réactive le mode standard
+    MAIL_SSL_TLS = False,
+    # -------------------------------
+    
     USE_CREDENTIALS = True,
     VALIDATE_CERTS = True
 )
